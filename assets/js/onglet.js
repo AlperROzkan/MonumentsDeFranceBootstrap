586713bdaf6a52b3a2c63c5afe27e15b6bc37067
OZKAN <alper.ozkan.p@gmail.com>
2019-03-25 21:57:16 +0100

    Vignette d'images

--- assets/js/onglet.js
+++ assets/js/onglet.js
@@ -5,3 +5,17 @@ $( document ).ready(function() {
         $( "#home-tab" ).trigger( "click" );
     }, 150);
 });
+
+
+$(function() {
+    var selectedClass = "";
+    $(".filter").click(function(){
+        selectedClass = $(this).attr("data-rel");
+        $("#gallery").fadeTo(100, 0.1);
+        $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+        setTimeout(function() {
+            $("."+selectedClass).fadeIn().addClass('animation');
+            $("#gallery").fadeTo(300, 1);
+        }, 300);
+    });
+});
