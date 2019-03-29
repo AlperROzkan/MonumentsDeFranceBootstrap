1827825eadeb789d057349f957385ba03229be19
OZKAN <alper.ozkan.p@gmail.com>
2019-03-29 10:44:00 +0100

    Version finale

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -5,12 +5,9 @@ $(function(){
         var winTop = $(window).scrollTop();
         if(winTop >= 30){
             $(".bg-light").addClass( "menuVolant" )
-            console.log("test");
 
         } else {
             $(".bg-light").removeClass( "menuVolant" )
-            console.log("tset");
-
         }
 
 
