d0beb73174cf9f8d6e32bb42ad3626485bf916e7
OZKAN <alper.ozkan.p@gmail.com>
2019-03-29 10:17:23 +0100

    css changes

--- assets/js/onglet.js
+++ assets/js/onglet.js
@@ -1,9 +1,36 @@
 $( document ).ready(function() {
 
+    $('.slider_one_big_picture').EasySlides({
+        'autoplay': true,
+        'stepbystep': false,
+        'show': 5,
+        'loop': false
+    })
+    $('.slider_one_big_2').EasySlides({
+        'autoplay': false,
+        'stepbystep': true,
+        'show': 5,
+        'loop': false
+    })
+    $('.slider_circle_10').EasySlides({
+        'autoplay': true,
+        'show': 13
+    })
+    $('.slider_four_in_line').EasySlides({
+        'autoplay': true,
+        'show': 9
+    })
+    $('.slider_clock').EasySlides({
+        'autoplay': true,
+        'stepbystep': false,
+        'show': 15
+    });
+
     $( "#profile-tab" ).trigger( "click" );
     setTimeout(()=> {
         $( "#home-tab" ).trigger( "click" );
     }, 150);
+
 });
 
 
